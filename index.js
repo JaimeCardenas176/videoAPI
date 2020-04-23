5d6c98421c8ba2a8cceb680341f219b91c52d5ce
Jaime Cárdenas Mesa <jaime.cardenas176@gmail.com>
2020-04-24 00:57:00 +0200

    second commit movi'es collection Mock and starting with the routes

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const { config } = require('./config/index');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("probando server con express")
-});
+const moviesApi = require('./router/movies');
 
-app.get('/json', (req, res) => {
-    res.json({
-        hello: 'world'
-    });
-});
+moviesApi(app);
+//app.get('/', (req, res) => {
+    //res.send("probando server con express")
+//});
+//
+//app.get('/json', (req, res) => {
+//    res.json({
+//        hello: 'world'
+//    });
+//});
 
 app.listen(config.port, () => {
     console.log(`Listening on http://localhost:${config.port}`);
