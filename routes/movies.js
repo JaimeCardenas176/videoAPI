094bfc49758f8d08e2b1e3b5d32098779590ff38
Jaime Cárdenas Mesa <jaime.cardenas176@gmail.com>
2020-04-27 02:52:23 +0200

    adding a cache layer and fixin database conection method

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+ const config = require('../config/index');
 const MoviesService = require('../services/movies');
 
 const { movieIdSchema, createMovieSchema, updateMovieSchema } = require('../utils/schemas/movie');
 const validationHandler = require('../utils/middlewares/validationHandler');
+
+const cacheResponse = require('../utils/cacheResponse');
+const { FIVE_MINUTES_IN_SECONDS, SIXTY_MINUTES_IN_SECONDS } = require('../utils/time');
 
 function moviesApi(app) {
     const router = express.Router();
@@ -12,6 +15,7 @@ function moviesApi(app) {
     const moviesService = new MoviesService();
 
     router.get('/', async function (req, res, next) {
+        //cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
         const { tags } = req.query;
         try {
             const movies = await moviesService.getMovies({ tags });
@@ -27,6 +31,7 @@ function moviesApi(app) {
 
 
     router.get('/:movieId', validationHandler({ movieId: movieIdSchema }, 'params'), async function (req, res, next) {
+        //cacheResponse(res, SIXTY_MINUTES_IN_SECONDS);
         const { movieId } = req.params;
         try {
             const movies = await moviesService.getMovie({ movieId });
